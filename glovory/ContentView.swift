79552fe1a8cbe401527a0ba7f1203d88f8767413
Albert Stanley <albertstanleyy@gmail.com>
2020-10-29 19:32:39 +0700

    Initial Commit

---
+++ glovory/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  glovory
+//
+//  Created by AlbertStanley on 29/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ glovory/glovoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  glovoryApp.swift
+//  glovory
+//
+//  Created by AlbertStanley on 29/10/20.
+//
+
+import SwiftUI
+
+@main
+struct glovoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
